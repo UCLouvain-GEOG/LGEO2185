3a81c0928946c57b695523b66f70f224cf24c0a4
Kristof Van Oost <kvanoost@wifi-secure1-785.sri.ucl.ac.be>
2026-02-06 15:10:15 +0100

    Remove large datafiles

--- Session 3/R/00_libraries.R
+++
@@ -1,6 +0,0 @@
-# install pacman if not already done
-if (!requireNamespace("pacman", quietly = TRUE)) install.packages("pacman")
-pacman::p_load(
-  tidyverse, tidyterra, scales, lattice, latticeExtra, sp, terra, geodata, rasterVis,
-  RColorBrewer, mapview, leaflet, leaflet.extras2, leafsync, viridis, plotly, here
-)
--- Session 3/R/00_parameters.R
+++
@@ -1,4 +0,0 @@
-geodata_default_path <- here("data", "raw")
-options(geodata_default_path = geodata_default_path)
-# check
-geodata_path()
